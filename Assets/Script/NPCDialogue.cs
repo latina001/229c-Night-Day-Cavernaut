a94898fe3b3e0bc02e32cd9239e7681b015b5e7f
kunni <nannaphat20160@gmail.com>
2026-05-02 20:22:56 +0700

    NPCDialogue

---
+++ Assets/Script/NPCDialogue.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour
+{
+    public GameObject dialogueUI;
+
+    bool playerInRange;
+
+    void Start()
+    {
+        if (dialogueUI != null)
+            dialogueUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleDialogue();
+        }
+    }
+
+    void ToggleDialogue()
+    {
+        if (dialogueUI == null) return;
+
+        bool isActive = dialogueUI.activeSelf;
+        dialogueUI.SetActive(!isActive);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Debug.Log("💬 Press E to talk");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+
+            if (dialogueUI != null)
+                dialogueUI.SetActive(false);
+        }
+    }
+}
